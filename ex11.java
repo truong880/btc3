34c4bfa3cb46c936624e7fbeee041a13fcff4efa
unknown <nguyendinhtruong15012006@gmail.com>
2024-10-08 08:28:47 +0700

    btc3

---
+++ ex11.java
@@ -0,0 +1,11 @@
+public class ex11 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i+"EVEN");
+            }else{
+                System.out.println(i+"ODD");
+            }
+        }
+    }
+}
---
+++ ex8.java
@@ -0,0 +1,10 @@
+public class ex8 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        double average = sum / 100.0;
+        System.out.println("Average:"+average);
+    }
+}
